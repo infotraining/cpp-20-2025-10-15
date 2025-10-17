4d2ec41ff1e8a6a0895071adb115e21054a1f5e2
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-10-17 11:18:47 +0000

    Lambda in C++20

--- templates/templates.cpp
+++ templates/templates.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <set>
 #include <array>
+#include <map>
 
 using namespace std::literals;
 
@@ -101,4 +102,93 @@ TEST_CASE("create_buffer")
 {
     auto small_buffer = create_buffer<128>();  // std::array<std::byte, 128>
     //auto large_buffer = create_buffer<1024>(); // std::vector<std::byte>
+}
+
+//////////////////////////////////////////////////////
+// Lambda features in C++20
+
+TEST_CASE("explicit params for generic lambdas")
+{
+    auto add_42 = []<std::integral T>(std::vector<T>& container) 
+        requires (sizeof(T) > 1)
+    {
+        container.push_back(42);
+    };
+
+    std::vector<int> vec;
+    add_42(vec);
+
+    CHECK(vec == std::vector{42});
+
+    // std::vector<uint8_t> vec_bytes;
+    // add_42(vec_bytes);
+
+    auto add_value = []<typename TItem>(auto& container, TItem&& item) {
+        container.push_back(std::forward<TItem>(item));
+    };
+
+    std::vector<std::string> words;
+    add_value(words, "text"s);
+}
+
+// closure class - klasa domknięcia
+class Lambda_472357645762
+{
+public:
+    auto operator()(int n) { return n * n; }
+};
+
+TEST_CASE("default init")
+{
+    auto closure = [](int n) { return n * n; };
+    auto closure_explain = Lambda_472357645762{};
+
+    CHECK(closure(4) == 16);
+    CHECK(closure_explain(5) == 25);
+
+    decltype(closure) another_closure{}; // since C++20
+
+    std::array data = {7, 6, 1, 0, 5};
+
+    auto cmp_by_value = [](int* a, int* b) { return *a < *b; };
+
+    std::set<int*, decltype(cmp_by_value)> mset = { &data[2], &data[1], &data[3], &data[0] };
+
+    for(auto* ptr : mset)
+    {
+        std::cout << *ptr << " ";
+    }
+    std::cout << "\n";
+}
+
+TEST_CASE("capturing sb")
+{
+    std::map<int, std::string> dict = { {0, "zero"}, {1, "one"} };
+
+    for(const auto& [key, value] : dict)
+    {
+        auto printer = [key, value] { 
+            std::cout << key << " - " << value << "\n";
+        };
+
+        printer();
+    }
+}
+
+auto create_caller(auto f, auto... args)
+{
+   return [f, ...args = std::move(args)]() -> decltype(auto) {
+        return f(args...);
+   }; 
+}
+
+int sum_of_three(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+TEST_CASE("capturing variadic pack")
+{
+    auto f = create_caller(sum_of_three, 1, 2, 3);
+    CHECK(f() == 6);
 }
